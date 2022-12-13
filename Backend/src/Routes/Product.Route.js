d24cd54ebfcf4ffb3885e84319e9dc241594ac31
raj-1313 <theromeo.1313@gmail.com>
2022-12-14 02:58:28 +0530

    added

--- Backend/src/Routes/Product.Route.js
+++ Backend/src/Routes/Product.Route.js
@@ -56,15 +56,69 @@ let data=  product.filter((el)=> el._id==id )
     }
 }
 })
+app.get("/men/:id",async (req,res)=>{
+    const id= req.params.id
+ let product= await productModel.findById(id)
+    res.send(product)
+})
 
 
-// app.get("/men/:id",async (req,res)=>{
-//     const id= req.params.id
-//  let product= await productModel.findById(id)
-//     res.send(product)
-// })
 
 
+
+
+app.get("/accessories",async (req,res)=>{        
+    let product= await productModel.find({category:"accessories"})
+        res.send(product)
+    })
+    
+    app.get("/accessories/filter",async (req,res)=>{    
+        let {type,id}= req.query
+    
+        if(type){
+        let product= await productModel.find({category:"accessories",type})
+        if(id){
+    let data=  product.filter((el)=> el._id==id )
+            res.send(data)
+        }else{
+            res.send(product)
+        }
+    }
+    })
+    app.get("/accessories/:id",async (req,res)=>{
+        const id= req.params.id
+     let product= await productModel.findById(id)
+        res.send(product)
+    })
+    
+    
+    
+    
+    app.get("/kids",async (req,res)=>{        
+    let product= await productModel.find({category:"kids_boy"})
+        res.send(product)
+    })
+    
+    app.get("/kids/filter",async (req,res)=>{    
+        let {type,id}= req.query
+    
+        if(type){
+        let product= await productModel.find({category:"kids",type})
+        if(id){
+    let data=  product.filter((el)=> el._id==id )
+            res.send(data)
+        }else{
+            res.send(product)
+        }
+    }
+    })
+    app.get("/kids/:id",async (req,res)=>{
+        const id= req.params.id
+     let product= await productModel.findById(id)
+        res.send(product)
+    })
+    
+    
 
 
 module.exports= app
